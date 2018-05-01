ed3756d0d9dcad9c5e893c07bdef33f8aa1126aa
W01fw00d <gabrm_91@hotmail.com>
2018-05-01 13:36:33 +0200

    4 assertions achieved

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -6,12 +6,37 @@ class Bottles
     
     def verse(n_bottles)
         n_bottles_after = n_bottles - 1
-        if n_bottles_after === 1
-            bottles_name_after = "bottle"
-        else
-            bottles_name_after = "bottles"
-        end
       
-        return "#{n_bottles} bottles of beer on the wall, #{n_bottles} bottles of beer.\nTake one down and pass it around, #{n_bottles_after} #{bottles_name_after} of beer on the wall.\n"
+        return "#{getBottles(n_bottles)} of beer on the wall, #{getBottles(n_bottles)} of beer.\nTake #{getDownBottle(n_bottles_after)} down and pass it around, #{getBottles(n_bottles_after)} of beer on the wall.\n"
+    end
+    
+    def getBottles(n_bottles)
+        if n_bottles === 0
+            bottles = "no more bottles"
+        else
+            bottles = "#{n_bottles} #{getBottlesName(n_bottles)}"
+        end
+            
+        return bottles
+    end
+    
+    def getBottlesName(n_bottles)
+        if n_bottles === 1
+            bottles_name = "bottle"
+        else
+            bottles_name = "bottles"
+        end
+        
+        return bottles_name
+    end
+    
+    def getDownBottle(n_bottles)
+        if n_bottles === 0
+            down_bottle = "it"
+        else
+            down_bottle = "one"
+        end
+            
+        return down_bottle
     end
 end
