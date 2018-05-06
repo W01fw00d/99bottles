d093dea475acbe1579a188c69ccb237a5bdb1cf9
W01fw00d <gabrm_91@hotmail.com>
2018-05-06 20:24:58 +0200

    5 assertions done

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -7,7 +7,21 @@ class Bottles
     def verse(n_bottles)
         n_bottles_after = n_bottles - 1
       
-        return "#{getBottles(n_bottles)} of beer on the wall, #{getBottles(n_bottles)} of beer.\nTake #{getDownBottle(n_bottles_after)} down and pass it around, #{getBottles(n_bottles_after)} of beer on the wall.\n"
+        return getFirstVerse(n_bottles) + getSecondVerse(n_bottles_after)
+    end
+    
+    def getFirstVerse(n_bottles)
+        return "#{getBottles(n_bottles)} of beer on the wall, #{getBottles(n_bottles)} of beer.\n".capitalize
+    end
+        
+    def getSecondVerse(n_bottles)
+        if n_bottles >= 0
+            verse = "Take #{getDownBottle(n_bottles)} down and pass it around, #{getBottles(n_bottles)} of beer on the wall.\n"
+        else
+            verse = "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+        end
+            
+        return verse
     end
     
     def getBottles(n_bottles)
